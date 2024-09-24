4cbe1d7c65a650ee60b234c471a2e4471c818381
Patryk Katek <pkatek@CLDSLRMBP231.local>
2024-09-24 13:21:28 +0200

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Spree::Core::Engine.add_routes do
+Spree::Core::Engine.routes.draw do
   namespace :gateway do
     post '/payu/comeback/:gateway_id/:order_id' => 'payu#comeback', :as => :payu_comeback
   end
