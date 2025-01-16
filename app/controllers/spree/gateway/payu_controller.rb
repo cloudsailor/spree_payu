2f30b6519cac212bd79d2216adccff9fd51dc7e8
vi4me <https://github.com/vi4me/>
2025-01-16 14:54:16 +0100

    Fix payu comeback error

--- app/controllers/spree/gateway/payu_controller.rb
+++ app/controllers/spree/gateway/payu_controller.rb
@@ -6,16 +6,17 @@ module Spree
     def comeback
       payment = Spree::Payment.find_by("public_metadata->>'token' = ?", params[:order][:orderId])
 
-      if payment&.state == 'checkout' &&
-         payment.payment_method.verify_transaction(params[:order][:status],
-                                                   payment,
-                                                   params[:order][:totalAmount],
-                                                   params[:order][:currencyCode],
-                                                   params[:order][:orderId]) &&
-          payment.order.update_with_updater!
-        head :ok
-      else
+      if payment.nil?
+        Rails.logger.error "Payment not found for order: #{params[:order][:orderId]}"
         head :unprocessable_entity
+      else payment.state == 'checkout' &&
+        payment.payment_method.verify_transaction(params[:order][:status],
+                                                 payment,
+                                                 params[:order][:totalAmount],
+                                                 params[:order][:currencyCode],
+                                                 params[:order][:orderId]) &&
+        payment.order.update_with_updater!
+        head :ok
       end
     end
   end
