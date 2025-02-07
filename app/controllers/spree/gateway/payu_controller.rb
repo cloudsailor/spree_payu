b4673e6b45ffcfcfd31fda7a52b28717ce21ab76
vi4me <https://github.com/vi4me/>
2025-02-07 16:32:14 +0100

    Fix payu#comeback if order_id is nil

--- app/controllers/spree/gateway/payu_controller.rb
+++ app/controllers/spree/gateway/payu_controller.rb
@@ -4,17 +4,20 @@ module Spree
     include Spree::Core::ControllerHelpers::Order
 
     def comeback
-      payment = Spree::Payment.find_by("public_metadata->>'token' = ?", params[:order][:orderId])
+      order_id = params.dig(:order, :orderId)
+      return head :unprocessable_entity if order_id.nil?
+
+      payment = Spree::Payment.find_by("public_metadata->>'token' = ?", order_id)
 
       if payment.nil?
-        Rails.logger.error "Payment not found for order: #{params[:order][:orderId]}"
+        Rails.logger.error "Payment not found for order: #{order_id}"
         head :unprocessable_entity
       else payment.state == 'checkout' &&
         payment.payment_method.verify_transaction(params[:order][:status],
                                                  payment,
                                                  params[:order][:totalAmount],
                                                  params[:order][:currencyCode],
-                                                 params[:order][:orderId]) &&
+                                                 order_id) &&
         payment.order.update_with_updater!
         head :ok
       end
