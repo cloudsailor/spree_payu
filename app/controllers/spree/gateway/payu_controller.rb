ee7a8c06dc3e22e64fae320081ce81675dbcea23
Tomasz Strzeszewski <tomasz.strzeszewski@yes.pl>
2025-08-08 15:16:06 +0200

    Add mapping based on paymentsFlow

--- app/controllers/spree/gateway/payu_controller.rb
+++ app/controllers/spree/gateway/payu_controller.rb
@@ -14,7 +14,11 @@ module Spree
         Rails.logger.error "Payment not found for order: #{payu_order_id}"
         head :unprocessable_entity
       end
-      payment.public_metadata['pay_method'] = payment.payment_method.fetch_transaction_pay_method(payu_order_id)
+
+      if payment.public_metadata['pay_method'].nil?
+        payment.public_metadata['pay_method'] = payment.payment_method.fetch_transaction_pay_method(payu_order_id)
+      end
+
       if payment.state == 'checkout' &&
         payment.payment_method.verify_transaction(params[:order][:status],
                                                  payment,
