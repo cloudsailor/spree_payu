e1a72529dfcdb0b24f61cb7586e549e54b1350dc
Patryk Kątek <pkatek@MacBook-Air-Patryk.local>
2026-01-13 12:23:56 +0100

    Fixed redirect if payment is as payu

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -19,17 +19,37 @@ module Spree
             redirect_to(spree.checkout_state_path(@order.token, @order.state)) && return
           end
 
-          if @order.completed?
-            track_checkout_completed
-            redirect_to @order.payments.last.public_metadata['payment_url'], allow_other_host: true
-          else
-            redirect_to spree.checkout_state_path(@order.token, @order.state)
-          end
+          redirect_after_checkout_update(@order)
         end
       else
         render :edit, status: :unprocessable_entity
       end
     end
+
+    private
+
+    def redirect_after_checkout_update(order)
+      if order.completed?
+        track_checkout_completed
+
+        if payu_redirect?(order)
+          redirect_to order.payments.last.public_metadata['payment_url'], allow_other_host: true, status: :see_other
+        else
+          redirect_to spree.checkout_complete_path(order.token), status: :see_other
+        end
+      else
+        redirect_to spree.checkout_state_path(order.token, order.state)
+      end
+    end
+
+    def payu_redirect?(order)
+      payment = order.payments.last
+      return false if payment.nil?
+
+      url = payment.public_metadata&.[]('payment_url')
+      url.present?
+    end
+  end
   end
 end
 
